0a32ef97eef55995aacdf2e44d0e391fcf358097
govindarajulu <govindarajulu90@gmail.com>
2012-05-22 19:40:23 +0530

    / ‘c/forkandexecvp.c’

--- c/forkandexecvp.c
+++ c/forkandexecvp.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <error.h>
+#include <string.h>
+#include <dlfcn.h>
+
+int spawn(const char* program,const char** arglist){
+    int pid;
+    pid=fork();
+    if(pid==0){
+        execvp(program,arglist);
+    } else {
+        return pid;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    char* arglist[]={"ls","-l"};
+    char* program="ls";
+    spawn(program,arglist);
+    return 0;
+}
  
