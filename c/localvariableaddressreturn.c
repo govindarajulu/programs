e5e29a8aedacdf7553a83df3b767442cf96fbaaf
govindarajulu <govindarajulu90@gmail.com>
2012-05-29 20:32:14 +0530

    / ‘c/localvariableaddressreturn.c’

--- c/localvariableaddressreturn.c
+++ c/localvariableaddressreturn.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+char* thread(void* nouse){
+    char a[10];
+    strcpy(a,"poison");
+    printf("a=%s\n",a);
+    return a;
+}
+
+main(){
+    char *a;
+    a=thread(NULL);
+    printf("%s\n",a);
+}
  
