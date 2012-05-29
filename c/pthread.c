ed63d476af8eb73cc48eff205d853d16aed44a33
govindarajulu <govindarajulu90@gmail.com>
2012-05-29 20:14:33 +0530

    / ‘c/pthread.c’

--- c/pthread.c
+++ c/pthread.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+void* thread(void* nouse){
+    while(1){
+        int i;
+        i++;
+    }
+    pthread_exit(NULL);
+}
+
+main(){
+    pthread_t id;
+    pthread_create(&id,NULL,*thread,NULL);
+    pthread_t id2;
+    pthread_create(&id2,NULL,*thread,NULL);
+    pthread_t id3;
+    pthread_create(&id3,NULL,*thread,NULL);
+    while(1){
+        pthread_join(id,NULL);
+        int j;
+        j++;
+        //printf("y");
+    }
+}
  
