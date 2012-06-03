866ed1d761d8957ef7deae0528833b2e6b538f87
govindarajulu <govindarajulu90@gmail.com>
2012-06-03 19:58:01 +0530

    / ‘c/pthread_self.c’ - ‘c/.pthread_self.c.kate-swp’

--- c/pthread_self.c
+++ c/pthread_self.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <error.h>
+void* thread(void* nouse){
+    printf("th in thread %d\n",pthread_self());
+    return NULL;
+}
+
+main(){
+    pthread_t th,th1;
+    pthread_create(&th,NULL,thread,NULL);
+    perror("pthread_create");
+    pthread_create(&th1,NULL,thread,NULL);
+    perror("pthread_create");
+    printf("th id in main()=%d\n",(int)th);
+    printf("th1 id in main()=%d\n",(int)th1);
+}
  
