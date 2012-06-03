8c1fc9ba4edac8a6bfb6202f5f83caa64ddcc963
govindarajulu <govindarajulu90@gmail.com>
2012-06-03 20:01:57 +0530

    / ‘c/pthread_attr.c’

--- c/pthread_attr.c
+++ c/pthread_attr.c
@@ -1,1 +1,31 @@
- 
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <error.h>
+void* thread(void* nouse){
+    while(1){
+        printf("%d\n",pthread_self());
+    }
+}
+
+main(){
+    pthread_t th;
+    pthread_attr_t th_attr;
+    struct sched_param th_sched;
+    th_sched.__sched_priority=0;
+    pthread_attr_init(&th_attr);
+    pthread_attr_setschedparam(&th_attr,&th_sched);
+
+
+    pthread_t th1;
+    pthread_attr_t th_attr1;
+    struct sched_param th_sched1;
+    th_sched.__sched_priority=10;
+    pthread_attr_init(&th_attr1);
+    pthread_attr_setschedparam(&th_attr1,&th_sched1);
+
+    pthread_create(&th,&th_attr,thread,NULL);
+    perror("pthread_create");
+    pthread_create(&th1,&th_attr1,thread,NULL);
+    pthread_join(th,NULL);
+} 
