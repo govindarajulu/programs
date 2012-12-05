879b48329bd433b54dd4463379642cb4f64ce0ec
govindarajulu <govindarajulu90@gmail.com>
2012-12-05 15:14:35 +0530

    fixed getopt_long args issue

--- c/getopt.c
+++ c/getopt.c
@@ -5,12 +5,12 @@ int main(int argc, char* argv[])
 {
     const char *short_opt="ho:";
     const struct option long_option[]={
-        {"help",0,NULL,'u'},
+        {"help",0,NULL,'h'},
         {"object",1,NULL,'o'}
     };
     int next_arg;
     do{
-        next_arg=getopt_long(argc,argv,NULL,long_option,NULL);
+        next_arg=getopt_long(argc,argv,short_opt,long_option,NULL);
         switch(next_arg)
         {
         case 'h':
@@ -22,8 +22,12 @@ int main(int argc, char* argv[])
         case '?':
             printf("?\n");
             break;
-        case -1:
+	case -1:
+	    break;
+        default:
+	    printf("usage: %s -h -o <object>\n",argv[0]);
             break;
+	    
         }
     }while(next_arg!=-1);
     return 0;
